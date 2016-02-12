d27c4981c054ddb827778e931a3bba364b5e9e25
Adam L Wolf <Adam.L.Wolf@Gmail.com>
2016-02-12 15:52:10 +0000

    Changed to Mailgun per lesson discussion

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,11 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-    
-    :address => 'smtp.sendgird.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :doamin => 'heroku.com',
-    :enable_startstls_auto => true
-    
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'sheltered-brushlands-47739.herokuapp.com',
+  :authentication => :plain,
 }
+ActionMailer::Base.delivery_method = :smtp
